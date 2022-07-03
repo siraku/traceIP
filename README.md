17bd115632aa001ee7f591804fe462b5af6137c1
siraku <slstc6666@yahoo.co.jp>
2022-07-03 18:15:41 +0900

    add code

---
+++ README.md
@@ -0,0 +1,14 @@
+# traceroute-maps
+This project to visualize the traceroute with maps
+
+## Installation
+
+```
+pip install -r requirements.txt
+```
+
+## Run
+
+```
+python main.py [hostname/domain]
+```
---
+++ trace.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+
+def traceroute(hostname):
+    '''
+    input: hostname/ip
+    return: list ip hop
+    '''
+    print(os.getcwd())
+    traceroute = subprocess.Popen(["traceroute",hostname],stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    ipList = []
+    for line in iter(traceroute.stdout.readline,b""):
+        line = line.decode("UTF-8")
+        IP = line.split("  ")
+        if len(IP)>1:
+            IP = IP[1].split("(")
+            if len(IP)>1:
+                IP = IP[1].split(")")
+                ipList.append(IP[0])
+    return ipList
